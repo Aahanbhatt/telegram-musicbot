6ad935e2728c81dc270297d89c07a40d73e6a8a3
aahan <aahanbhatt.stkabir@gmail.com>
2016-09-29 03:28:41 +0530

    Final bot

--- bot.py
+++ bot.py
@@ -34,7 +34,7 @@ def start(bot, update):
     chat_id = update.message.chat_id
 
     bot.sendMessage(chat_id,
-                    text="Hello, please type a song name to start downloading")
+                    text="Hello, please type a song name to start download")
 
 
 def admin(bot, update):
@@ -45,7 +45,7 @@ def admin(bot, update):
         users = len(select(u.user_id for u in User))
         last5 = '\n'.join(select(u.title for u in User)[:][-5:])
 
-    if username == 'TafarelYan':
+    if username == 'Humblefool':
         bot.sendMessage(chat_id,
                         text="{} users registered.\n\n{}".format(users, last5))
 
@@ -56,11 +56,11 @@ def music(bot, update):
     username = update.message.chat.username
     first_name = update.message.from_user.first_name
     last_name = update.message.from_user.last_name
-
     chat_id = update.message.chat_id
     text = update.message.text
 
-    title, video_url = search(text)
+    title, video_url, song = search(text)
+    lyrics = get_lyrics(text)
     with db_session:
         User(user_id=user_id,
              username=username,
@@ -70,18 +70,18 @@ def music(bot, update):
              video_url=video_url)
 
     bot.sendMessage(chat_id,
-                    text="Request received, downloading now...\nWhile it may take some time, you can perform 10 situps")
+                    text="Request received, downloading now...")
 
     download(title, video_url)
     bot.sendAudio(chat_id,
                   audio=open(title + '.mp3', 'rb'),
                   title=title)
+    bot.sendMessage(chat_id, text=lyrics)
     os.remove(title + '.mp3')
 
 
 def search(text):
     query = '+'.join(text.lower().split())
-
 
     url = 'https://www.youtube.com/results?search_query=' + query
     content = urlopen(url).read()
@@ -90,7 +90,7 @@ def search(text):
     title = tag.text
     video_url = 'https://www.youtube.com' + tag.get('href')
 
-    return title, video_url
+    return title, video_url, text
 
 
 def download(title, video_url):
@@ -104,6 +104,35 @@ def download(title, video_url):
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([video_url])
+
+def get_lyrics(song):
+
+    t=song.split()
+    x=len(t)
+    #print x
+    q=t[0]
+    for i in range(1,x):
+        q = q + "+" + t[i]
+
+    url="http://search.azlyrics.com/search.php?q="+q
+    s= urlopen(url).read()
+
+    soup=BeautifulSoup(s,'html.parser')
+
+
+    productLinks = [td.a for td in soup.findAll('td', {'class':"text-left visitedlyr"})]
+
+    for link in productLinks:
+
+        url1= link['href']
+
+    s1=urlopen(url1).read()
+
+    soup1=BeautifulSoup(s1,'html.parser')
+    for link in soup1.find_all('div',{'class':None}):
+        fin_lyr = (link.text)
+
+    return fin_lyr
 
 
 dp.add_handler(CommandHandler("start", start))
